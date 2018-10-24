63b9daaea5b9a443bf5e843489572f729c9e4a90
wudidada <hackerjiang@gmail.com>
2018-10-24 23:03:48 +0800

    third commit

--- src/com/jzb/leetcode/MedianOfTwoSortedArrays.java
+++ src/com/jzb/leetcode/MedianOfTwoSortedArrays.java
@@ -2,38 +2,45 @@ package com.jzb.leetcode;
 
 public class MedianOfTwoSortedArrays {
 
-	public static double findMedianSortedArrays(int[] A, int[] B) {
-        int m = A.length;
-        int n = B.length;
-        if (m > n) { // to ensure m<=n
-            int[] temp = A; A = B; B = temp;
-            int tmp = m; m = n; n = tmp;
-        }
-        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
-        while (iMin <= iMax) {
-            int i = (iMin + iMax) / 2;
-            int j = halfLen - i;
-            if (i < iMax && B[j-1] > A[i]){
-                iMin = i + 1; // i is too small
-            }
-            else if (i > iMin && A[i-1] > B[j]) {
-                iMax = i - 1; // i is too big
-            }
-            else { // i is perfect
-                int maxLeft = 0;
-                if (i == 0) { maxLeft = B[j-1]; }
-                else if (j == 0) { maxLeft = A[i-1]; }
-                else { maxLeft = Math.max(A[i-1], B[j-1]); }
-                if ( (m + n) % 2 == 1 ) { return maxLeft; }
+	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
 
-                int minRight = 0;
-                if (i == m) { minRight = B[j]; }
-                else if (j == n) { minRight = A[i]; }
-                else { minRight = Math.min(B[j], A[i]); }
-
-                return (maxLeft + minRight) / 2.0;
-            }
+        if (m > n) {
+            int[] temp = nums1; nums1 = nums2; nums2 = temp;
+            m = m + n; n = m - n;
+            m -= n;
         }
+
+        int imin = 0, imax = m;
+
+        while (imin <= imax) {
+			int i = (imin + imax) / 2;
+			int j = (m + n + 1) / 2 - i;
+        	if (i > imin && nums2[j - 1] > nums1[i]) {
+        		imin++;
+	        } else if (i < imax && nums1[i - 1] > nums2[j]) {
+        		imax--;
+	        } else {
+        		boolean even = (m + n) % 2 == 0;
+        		int maxLeft = 0;
+        		if (i == 0) {
+        			maxLeft = nums2[j - 1];
+		        } else if (j == 0) {
+        			maxLeft = nums1[i -1];
+		        }
+
+		        if ( (m + n) % 2 == 1 ) { return maxLeft; }
+
+		        int minRight = 0;
+		        if (i == m) { minRight = nums2[j]; }
+		        else if (j == n) { minRight = nums1[i]; }
+		        else { minRight = Math.min(nums2[j], nums1[i]); }
+
+		        return (maxLeft + minRight) / 2.0;
+	        }
+        }
+
         return 0.0;
     }
 
@@ -57,8 +64,9 @@ public class MedianOfTwoSortedArrays {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int[] nums1 = new int[] {1};
+		int[] nums1 = new int[] {1,2};
 		int[] nums2 = new int[] {};
+
 		
 		System.out.println(findMedianSortedArrays(nums1, nums2));
 	}
